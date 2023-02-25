ed9db49e6ce15370a72931578e0686039e40b6cf
Antonio Estevez <aestgar62gmail.com>
2023-02-25 12:27:08 +0000

    Schema.org and VC-examples added

--- src/lib.rs
+++ src/lib.rs
@@ -5,4 +5,11 @@ pub const DID_RESOLUTION_V1: &str = include_str!("../w3c-did-resolution-v1.jsonl
 pub const DID_V1: &str = include_str!("../w3c-did-v1.jsonld");
 
 /// <https://www.w3.org/TR/vc-data-model/>
-pub const VERIFIABLE_CREDENTIALS_V1: &str = include_str!("../w3c-verifiable-credentials-v1.jsonld");
+pub const CREDENTIALS_V1: &str = include_str!("../w3c-verifiable-credentials-v1.jsonld");
+
+/// <https://www.w3.org/2018/credentials/examples/v1>
+pub const CREDENTIALS_EXAMPLES_V1: &str =
+    include_str!("../w3c-verifiable-credentials-examples-v1.jsonld");
+
+/// <https://schema.org/>
+pub const SCHEMA_ORG: &str = include_str!("../schema.org.jsonld");
--- update.sh
+++ update.sh
@@ -5,4 +5,6 @@ exec curl \
 	https://www.w3.org/ns/did/v1 -o w3c-did-v1.jsonld \
 	https://w3id.org/did-resolution/v1 -o w3c-did-resolution-v1.jsonld \
 	https://www.w3.org/2018/credentials/v1 -o w3c-verifiable-credentials-v1.jsonld \
+	https://www.w3.org/2018/credentials/examples/v1 -o w3c-verifiable-credentials-examples-v1.jsonld \
+	https://schema.org/docs/jsonldcontext.jsonld -o schema.org.jsonld \
 	-L
